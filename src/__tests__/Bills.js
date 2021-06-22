0162c3ba801ec3663e228abf93b821f8af6143d0
Maxime-Moilliet <mlltmax66@gmail.com>
2021-06-22 16:26:06 +0200

    test BillsUI

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,14 +1,36 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import Router from '../app/Router.js'
+import Firestore from "../app/Firestore.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { ROUTES_PATH } from "../constants/routes.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
+      jest.mock("../app/Firestore")
+      Firestore.bills = () => ({get: jest.fn().mockResolvedValue()})
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock
+      })
+
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee"
+        })
+      )
+
+      const pathname = ROUTES_PATH["Bills"]
+      Object.defineProperty(window, "location", { value: { hash: pathname } })
+      document.body.innerHTML = `<div id="root"></div>`
+      Router()
+      expect(
+        screen.getByTestId("icon-window").classList.contains("active-icon")
+      ).toBe(true)
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
@@ -18,4 +40,20 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+
+  describe("When I am on BillsUI Page", () => {
+    test("Then BillsUI and have loading defined true", () => {
+      const html = BillsUI({ data: [], loading: true })
+      document.body.innerHTML = html
+      expect(screen.getByText(/Loading.../i)).toBeTruthy()
+    })
+
+    test("Then BillUsI and have error defined true", () => {
+      const html = BillsUI({ data: [], error: true })
+      document.body.innerHTML = html
+      const message = screen.getByTestId("error-message")
+      expect(message).toBeTruthy()
+    })
+  })
+
 })
